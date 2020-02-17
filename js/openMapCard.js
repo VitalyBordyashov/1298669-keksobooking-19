48f9132f6d34ee9ddd3e79c2149445f476109f70
VitalyBordyashov <vital.bordyashov@mail.ru>
2020-02-17 15:12:41 +0300

    открытие и закрытие карт

--- js/openMapCard.js
+++ js/openMapCard.js
@@ -1,10 +1,21 @@
 'use strict';
 // обработчик клика на метку
 (function () {
+  var mapPins = document.querySelector('.map__pins');
+  var popupCard = document.querySelector('.map__card');
   mapPins.addEventListener('click', function (evt) {
-    if (evt.target.hasAttribute('data_click')) {
-      var dataClick = evt.target.getAttribute('data_click');
-      popupCard.querySelector('article[data_click="dataClick"]').classList.remove('hidden');
+    if (evt.target.nodeName != 'BUTTON') {
+      var ParentTarget = evt.target.parentNode;
+    } else {
+      ParentTarget = evt.target;
+    }
+    if (ParentTarget.hasAttribute('data_click')) {
+      var dataClick = ParentTarget.getAttribute('data_click');
+      var mapCard = document.querySelectorAll('.map .map__card');
+      for (var i = 0; i < mapCard.length; i++) {
+        mapCard[i].classList.add('hidden');
+      }
+      document.querySelector('article[data_click="'+dataClick+'"]').classList.remove('hidden');
     }
 });
 })();
--- js/popurClose.js
+++ js/popurClose.js
@@ -1,10 +1,20 @@
 'use strict';
-// закрытие объявления
+// обработчик клика на метку
 (function () {
-  var popupCard = document.querySelector('.map__card');
-  var closeCard = popupCard.querySelector('.popup__close');
-  closeCard.addEventListener('click', function(){
-    popupCard.classList.add('hidden');
-  })
+  var map = document.querySelector('.map');
+  map.addEventListener('click', function (evt) {
+    if (evt.target.className === 'popup__close') {
+      var ParentTarget = evt.target.parentNode;
+      ParentTarget.classList.add('hidden');
+    } else false;
+  });
+  map.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter'||evt.key === 'Escape') {
+      var mapCard = document.querySelectorAll('.map__card');
+      for (var i = 0; i < mapCard.length; i++) {
+        mapCard[i].classList.add('hidden');
+      }
+    }
+  });
 })();
 
