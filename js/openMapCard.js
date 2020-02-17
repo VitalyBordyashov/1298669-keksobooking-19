7ecce25a8c786be9a203e611fca0fa805f74cda5
Vitaliy Bordyashov <vital.bordyashov>
2020-02-17 21:38:11 +0300

    доработка открытия карточки

--- js/openMapCard.js
+++ js/openMapCard.js
@@ -3,6 +3,7 @@
 (function () {
   var mapPins = document.querySelector('.map__pins');
   var popupCard = document.querySelector('.map__card');
+
   mapPins.addEventListener('click', function (evt) {
     if (evt.target.nodeName != 'BUTTON') {
       var ParentTarget = evt.target.parentNode;
@@ -18,4 +19,20 @@
       document.querySelector('article[data_click="'+dataClick+'"]').classList.remove('hidden');
     }
 });
+  var map = document.querySelector('.map');
+  map.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Enter'||evt.target.hasAttribute('data_click')||evt.target.nodeName != 'BUTTON') {
+      var ParentTarget = evt.target.parentNode;
+    } else {
+      ParentTarget = evt.target;
+    }
+    if (ParentTarget.hasAttribute('data_click')) {
+      var dataClick = ParentTarget.getAttribute('data_click');
+      var mapCard = document.querySelectorAll('.map .map__card');
+      for (var i = 0; i < mapCard.length; i++) {
+        mapCard[i].classList.add('hidden');
+      }
+      document.querySelector('article[data_click="'+dataClick+'"]').classList.remove('hidden');
+    }
+  });
 })();
