78d0a2936af1e29599281ebb7688a58381e98fd6
VitalyBordyashov <vital.bordyashov@mail.ru>
2020-02-19 15:22:18 +0300

    модули созданы

--- js/checktime.js
+++ js/checktime.js
@@ -4,9 +4,9 @@
   var timein = document.querySelector('#timein');
   var timeout = document.querySelector('#timeout');
   timein.addEventListener('change', function () {
-    timeout.value = timein.value;  
+    timeout.value = timein.value;
   });
   timeout.addEventListener('change', function () {
-    timein.value = timeout.value;  
+    timein.value = timeout.value;
   });
 })();
