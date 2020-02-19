313f5eb561dceae9167d67bf17a4216a4605ce86
VitalyBordyashov <vital.bordyashov@mail.ru>
2020-02-19 14:35:32 +0300

    готово 43

--- js/checktime.js
+++ js/checktime.js
@@ -1,29 +1,12 @@
 'use strict';
 // валидация времени заезда/выезда
 (function () {
-  var timein = document.querySelector('#timein').options;
-  var timeout = document.querySelector('#timeout').options;
-  var timeinChangeHandler = function () {
-    for (var i = 0; i < timein.length; i++) {
-      if (timein[i].text === 'После 12') {
-      timeout[i].options.text = 'Выезд до 12';
-    } else if (timein[i].options.text === 'После 13') {
-      timeout[i].options.text = 'Выезд до 13';
-    } else if (timein[i].options.text === 'После 14') {
-      timeout[i].options.text = 'Выезд до 14';
-    }
-    }
-  };
-  var timeoutChangeHandler = function () {
-    if (timeout.options.value === 'Выезд до 12') {
-      timein.setAttribute('value', 'Выезд до 12');
-    } else if (timeout.options.options.value === 'Выезд до 13') {
-      timein.setAttribute('value', 'После 13');
-    } else if (timeout.options.value === 'Выезд до 14') {
-      timein.setAttribute('value', 'После 14');
-    }
-  }
-
-  timein.addEventListener('change', timeinChangeHandler);
-  timeout.addEventListener('change', timeoutChangeHandler);
+  var timein = document.querySelector('#timein');
+  var timeout = document.querySelector('#timeout');
+  timein.addEventListener('change', function () {
+    timeout.value = timein.value;  
+  });
+  timeout.addEventListener('change', function () {
+    timein.value = timeout.value;  
+  });
 })();
--- js/validationForm.js
+++ js/validationForm.js
@@ -4,22 +4,22 @@
   var typeHousing = document.querySelector('#type');
   var costHousing = document.querySelector('#price');
   var selectChangeHandler = function () {
-    if (typeHousing.value === 'Бунгало') {
+    if (typeHousing.value === 'bungalo') {
       costHousing.setAttribute('min', '0');
       costHousing.setAttribute('placeholder', '0');
-    } else if (typeHousing.value === 'Квартира') {
+    } else if (typeHousing.value === 'flat') {
       costHousing.setAttribute('min', '1000');
       costHousing.setAttribute('placeholder', '1000');
       if (costHousing.value < 1000) {
         costHousing.setCustomValidity('Минимальная цена за квартиру 1000');
       }
-    } else if (typeHousing.value === 'Дом') {
+    } else if (typeHousing.value === 'house') {
       costHousing.setAttribute('min', '5000');
       costHousing.setAttribute('placeholder', '5000');
       if (costHousing.value < 5000) {
         costHousing.setCustomValidity('Минимальная цена за дом 5000');
       }
-    } else if (typeHousing.value === 'Дворец') {
+    } else if (typeHousing.value === 'palace') {
       costHousing.setAttribute('min', '10000');
       costHousing.setAttribute('placeholder', '10000');
       if (costHousing.value < 10000) {
